4d32c67c99e36ad8dfa6d9cb396b213458c02ede
YvesMu <95441649+YvesMu@users.noreply.github.com>
2025-06-23 16:46:19 +0200

    Ajout modification

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-// filepath: c:\Users\CDXTELECOM\Downloads\Fluent-IA\fullstack-app\backend\src\app.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
