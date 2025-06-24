9ca731618ea2c169279f556973516733a9bfe1c6
Dambh45 <148864933+Dambh45@users.noreply.github.com>
2025-06-24 14:50:56 +0200

    Ajout ia recherche prestation + ia génération prestation

--- backend/src/ia/ia.controller.ts
+++ backend/src/ia/ia.controller.ts
@@ -1,13 +1,79 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { IaService } from './ia.service';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
 
 @Controller('ia')
 export class IaController {
   constructor(private readonly iaService: IaService) {}
 
-  @Get('generate')
-  async generate(@Query('prompt') prompt: string) {
-    const result = await this.iaService.generateText(prompt);
-    return { result };
+  @Post('prestations')
+  async getPrestations(@Body() body: { userRequest: string }) {
+    const organisations = [{
+      id: "1",
+      nom: "Entreprise1",
+      secteur: "Informatique",
+      pays: "France",
+      description: "Entreprise1 vous propose des solutions informatique",
+      prestations: [
+        "graphiste",
+        "developpeur java"
+      ]
+    }, {
+      id: "2",
+      nom: "Entreprise2",
+      secteur: "Alimentaire",
+      pays: "France",
+      description: "Entreprise2 vous propose des services alimentaire",
+      prestations: [
+        "patisserie"
+      ]
+    }];
+    const prestations = [{
+      id: "1",
+      nom: "graphiste",
+      descriptif: "Je vous propose un service de graphiste, pour créer vos logos, toute idée créative",
+      localisation: "Paris",
+      physique: false,
+      tags: [
+        "graphiste",
+        "logo"
+      ]
+    }, {
+      id: "2",
+      nom: "developpeur java",
+      descriptif: "Je vous propose un service complet pour la création de vos solution informatique en java",
+      localisation: "Paris",
+      physique: false,
+      tags: [
+        "developpeur java",
+        "java"
+      ]
+    }, {
+      id: "3",
+      nom: "patisserie",
+      descriptif: "Je vous propose de vous faire des patisserie ansi que tout ce qui concerne boulangerie",
+      localisation: "Paris",
+      physique: true,
+      tags: [
+        "patisserie",
+        "boulanger"
+      ]
+    }];
+    const prestationIDS = await this.iaService.getPrestations(body.userRequest, organisations, prestations);
+    const responsePrestations = prestations.filter(prestation =>
+      prestationIDS.includes(prestation.id),
+    );
+    return responsePrestations;
   }
+
+  @Get('form')
+  async generateForm() {
+    return this.iaService.generateForm();
+  }
+
+  /*@Post('generate')
+  async generateDemand(@Body body: { prestation: any }) {
+    return this.iaService;
+  }*/
 }
--- backend/src/ia/ia.service.ts
+++ backend/src/ia/ia.service.ts
@@ -5,13 +5,14 @@ import axios from 'axios';
 export class IaService {
   private readonly apiUrl = 'https://openrouter.ai/api/v1/chat/completions';
   private readonly apiKey = process.env.IA_API_KEY;
+  private userDemand = "";
 
-  async generateText(prompt: string) {        
+  async generateText(prompt: string) {
     const headers = {
       'Authorization': `Bearer ${this.apiKey}`,
       'Content-Type': 'application/json',
     };
-    
+
     const data = {
       model: 'mistralai/mistral-small-3.2-24b-instruct:free',
       messages: [
@@ -20,13 +21,13 @@ export class IaService {
           content: [
             {
               type: 'text',
-              text: 'Salut',
-            }
+              text: prompt,
+            },
           ],
         },
       ],
     };
-    
+
     try {
       const response = await axios.post(this.apiUrl, data, { headers });
       console.log('Response:', response.data.choices[0].message.content);
@@ -35,5 +36,58 @@ export class IaService {
       console.error('Error calling OpenRouter API:', error.response?.data || error.message);
       throw error;
     }
+  }
+
+  async getPrestations(userRequest: string, organisations: any[], prestations: any[]) {
+    console.log(userRequest, organisations, prestations);
+    this.userDemand = userRequest;
+    const prompt = `
+      L'utilisateur demande un type de service. Voici les détails de l'organisation et des prestations disponibles :
+
+      Organisations :
+      ${organisations.map(org => `- ${org.nom} : [id: ${org.id}, nom: ${org.nom}, secteur: ${org.secteur}, pays: ${org.pays}, description: ${org.description}, prestations: [${org.prestations.map(presta => `${presta}`).join(', ')}]]`).join('\n')}
+
+      Prestations :
+      ${prestations.map(prestation => `- ${prestation.nom} : [id: ${prestation.id}, nom: ${prestation.nom}, descriptif: ${prestation.descriptif}, localisation: ${prestation.localisation}, physique: ${prestation.physique}, tags: [${prestation.tags.map(tag => `${tag}`).join(', ')}]]`).join('\n')}
+
+      Utilisateur : ${userRequest}
+
+      Réponds avec une liste de prestations pertinentes pour la demande de l'utilisateur.
+      Tu me répondra au format UNIQUEMENT sous le format [prestationID, ...]
+    `;
+
+    const response = await this.generateText(prompt);
+    return response.choices[0].message.content;
+  }
+
+  async generateForm() {
+    console.log(this.userDemand);
+    const prompt = `
+      L'utilisateur à fais une demande de prestation :
+      ${this.userDemand}
+
+      Génère un formulaire dynamique demandant les informations nécessaires pour répondre à la problématique de l'utilisateur. Par exemple, pour un logo, demande des informations sur les couleurs, les formes, etc.
+      Tu me répondra UNIQUEMENT sous le format JSON comme l'exemple suivant :  
+      [{
+        type: "text",
+        name: "couleur",
+        label: "Couleur"
+      }, {
+        type: "select",
+        name: "forme",
+        label: "Forme",
+        options: [{
+            name: "Rond",
+            value: "rond",
+          }, {
+            name: "Carré",
+            value: "carre"
+          }
+        ]
+      }]
+    `;
+
+    const response = await this.generateText(prompt);
+    return response.choices[0].message.content;
   }
 }
